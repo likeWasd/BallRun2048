4bcf810aff286356c497b29365aa32932702accf
likeWasd <mana.dog0077@gmail.com>
2024-10-06 13:13:23 +0900

    Sphere同士がぶつかったら、Playerじゃない方のSphereが消えるようにした
    ・同時に、PlayerのSphereの数字や色を変えるようにした
    ・ステージを少し作った

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerMove : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class PlayerMove : MonoBehaviour
     int moveSpeedF;
     // プレイヤーが進むスピード(LRはLeftRight)
     int moveSpeedLR;
+    [SerializeField] TextMeshPro sphereNumber;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +28,55 @@ public class PlayerMove : MonoBehaviour
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += moveSpeedLR * transform.right * Time.deltaTime;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Sphere_2") && sphereNumber.text == "2")
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+            sphereNumber.text = "4";
+        }
+        else if (collision.gameObject.CompareTag("Sphere_4") && sphereNumber.text == "4")
+        {
+            GetComponent<Renderer>().material.color = Color.white;
+            sphereNumber.text = "8";
+        }
+        else if (collision.gameObject.CompareTag("Sphere_8") && sphereNumber.text == "8")
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+            sphereNumber.text = "16";
+        }
+        else if (collision.gameObject.CompareTag("Sphere_16") && sphereNumber.text == "16")
+        {
+            GetComponent<Renderer>().material.color = Color.white;
+            sphereNumber.text = "32";
+        }
+        else if (collision.gameObject.CompareTag("Sphere_32") && sphereNumber.text == "32")
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+            sphereNumber.text = "64";
+        }
+        else if (collision.gameObject.CompareTag("Sphere_64") && sphereNumber.text == "64")
+        {
+            GetComponent<Renderer>().material.color = Color.white;
+            sphereNumber.text = "128";
+        }
+        else if (collision.gameObject.CompareTag("Sphere_128") && sphereNumber.text == "128")
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+            sphereNumber.text = "256";
+        }
+        else if (collision.gameObject.CompareTag("Sphere_256") && sphereNumber.text == "256")
+        {
+            GetComponent<Renderer>().material.color = Color.white;
+            sphereNumber.text = "512";
+        }
+        else if (collision.gameObject.CompareTag("Sphere_512") && sphereNumber.text == "512")
+        {
+            GetComponent<Renderer>().material.color = Color.white;
+            sphereNumber.text = "1024";
         }
     }
 }
