c434502299d90ae2f1a92e02c9c0511aefdede3d
likeWasd <mana.dog0077@gmail.com>
2024-10-20 13:15:41 +0900

    SphereManagerのDestroy関数をPlayerMoveで処理するようにした
    ・ステージを拡張した
    ・マテリアルを配列で変更するようにした(まだ途中)
    ・Cubeを使ってトゲを作った(これも途中)

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -10,6 +10,8 @@ public class PlayerMove : MonoBehaviour
     // プレイヤーが進むスピード(LRはLeftRight)
     int moveSpeedLR;
     [SerializeField] TextMeshPro sphereNumber;
+    int number;
+    [SerializeField] Material[] numberMaterials = new Material[10];
     // Start is called before the first frame update
     void Start()
     {
@@ -33,50 +35,64 @@ public class PlayerMove : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
+        if (collision.gameObject.CompareTag("Thorn"))
+        {
+            number = int.Parse(sphereNumber.text) / 2;
+            sphereNumber.text = number.ToString();
+        }
         if (collision.gameObject.CompareTag("Sphere_2") && sphereNumber.text == "2")
         {
-            GetComponent<Renderer>().material.color = Color.red;
+            Destroy(collision.gameObject);
+            GetComponent<Renderer>().material = numberMaterials[2];
             sphereNumber.text = "4";
         }
         else if (collision.gameObject.CompareTag("Sphere_4") && sphereNumber.text == "4")
         {
-            GetComponent<Renderer>().material.color = Color.white;
+            Destroy(collision.gameObject);
+            GetComponent<Renderer>().material = numberMaterials[3];
             sphereNumber.text = "8";
         }
         else if (collision.gameObject.CompareTag("Sphere_8") && sphereNumber.text == "8")
         {
-            GetComponent<Renderer>().material.color = Color.red;
+            Destroy(collision.gameObject);
+            GetComponent<Renderer>().material = numberMaterials[4];
             sphereNumber.text = "16";
         }
         else if (collision.gameObject.CompareTag("Sphere_16") && sphereNumber.text == "16")
         {
-            GetComponent<Renderer>().material.color = Color.white;
+            Destroy(collision.gameObject);
+            GetComponent<Renderer>().material = numberMaterials[5];
             sphereNumber.text = "32";
         }
         else if (collision.gameObject.CompareTag("Sphere_32") && sphereNumber.text == "32")
         {
-            GetComponent<Renderer>().material.color = Color.red;
+            Destroy(collision.gameObject);
+            GetComponent<Renderer>().material = numberMaterials[6];
             sphereNumber.text = "64";
         }
         else if (collision.gameObject.CompareTag("Sphere_64") && sphereNumber.text == "64")
         {
-            GetComponent<Renderer>().material.color = Color.white;
+            Destroy(collision.gameObject);
+            GetComponent<Renderer>().material = numberMaterials[7];
             sphereNumber.text = "128";
         }
         else if (collision.gameObject.CompareTag("Sphere_128") && sphereNumber.text == "128")
         {
-            GetComponent<Renderer>().material.color = Color.red;
+            Destroy(collision.gameObject);
+            GetComponent<Renderer>().material = numberMaterials[8];
             sphereNumber.text = "256";
         }
         else if (collision.gameObject.CompareTag("Sphere_256") && sphereNumber.text == "256")
         {
-            GetComponent<Renderer>().material.color = Color.white;
+            Destroy(collision.gameObject);
+            GetComponent<Renderer>().material = numberMaterials[9];
             sphereNumber.text = "512";
         }
         else if (collision.gameObject.CompareTag("Sphere_512") && sphereNumber.text == "512")
         {
-            GetComponent<Renderer>().material.color = Color.white;
-            sphereNumber.text = "1024";
+            Destroy(collision.gameObject);
+            GetComponent<Renderer>().material = numberMaterials[10];
+            sphereNumber.text = "1k";
         }
     }
 }
