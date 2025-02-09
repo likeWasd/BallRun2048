7d99c21ace0646962a65006d4fed3e928a61b601
likeWasd <mana.dog0077@gmail.com>
2025-02-09 11:45:14 +0900

    ステージの生成方法を見直している途中

--- Assets/Scripts/RandomStageGenerator.cs
+++ Assets/Scripts/RandomStageGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,9 +11,12 @@ public class RandomStageGenerator : MonoBehaviour
     [SerializeField] GameObject wallPack;
     GameObject cloneObject;
     float[][] objectDatas;
+    int iLength;
+    System.Random rand = new System.Random();
     // Start is called before the first frame update
     void Start()
     {
+        /*
         objectDatas = new float[][]
         {
             new float[] {-3f, 20f, 1f, 0f},
@@ -22,7 +26,7 @@ public class RandomStageGenerator : MonoBehaviour
             new float[] {-3f, 50f, 1f, 0f},
             new float[] {0f, 50f, 1f, 0f},
             new float[] {3f, 50f, 3f, 0f},
-            new float[] {75f, 4f, 1f}
+            new float[] {0f, 75f, 4f, 1f}
         };
         for (int i = 0; i < 4; i++)
         {
@@ -46,14 +50,14 @@ public class RandomStageGenerator : MonoBehaviour
             };
         }
         objectDatas[34] = new float[] { -3f, 135f, 5f, 0f };
-        objectDatas[35] = new float[] { -3f, 135f, 5 };
-        objectDatas[36] = new float[] { -1f, 135f, 6 };
-        objectDatas[37] = new float[] { 1f, 135f, 9 };
-        objectDatas[38] = new float[] { 3f, 135f, 6 };
-        objectDatas[39] = new float[] { -3f, 145f, 9 };
-        objectDatas[40] = new float[] { -1f, 145f, 6 };
-        objectDatas[41] = new float[] { 1f, 145f, 5 };
-        objectDatas[42] = new float[] { 3f, 145f, 6 };
+        objectDatas[35] = new float[] { -3f, 135f, 5f, 0f };
+        objectDatas[36] = new float[] { -1f, 135f, 6f, 0f };
+        objectDatas[37] = new float[] { 1f, 135f, 9f, 0f };
+        objectDatas[38] = new float[] { 3f, 135f, 6f, 0f };
+        objectDatas[39] = new float[] { -3f, 145f, 9f, 0f };
+        objectDatas[40] = new float[] { -1f, 145f, 6f, 0f };
+        objectDatas[41] = new float[] { 1f, 145f, 5f, 0f };
+        objectDatas[42] = new float[] { 3f, 145f, 6f, 0f };
         for (int i = 0; i < 4; i++)
         {
             objectDatas[43 + i] = new float[] { i * 2f - 4f, 155f, 7f, 0f };
@@ -62,44 +66,84 @@ public class RandomStageGenerator : MonoBehaviour
         {
             objectDatas[47 + i] = new float[] { i * 2f - 3f, 165f, 8f, 0f };
         }
-        objectDatas[51] = new float[] { 190f, 8f, 1f };
-        objectDatas[52] = new float[] { 250f, 7f, 1f };
+        objectDatas[51] = new float[] { 0f, 190f, 8f, 1f };
+        objectDatas[52] = new float[] { 0f, 250f, 7f, 1f };
+        */
+        iLength = rand.Next(16, 26);
+        for (int i = 1; i < iLength; i++)
+        {
+            if (i == iLength - 1)
+            {
+                GenerateSphere(0, 20f + (i - 1) * 10, 2f, 1f);
+            }
+            else
+            {
+                if (i <= 4)
+                {
+                    GenerateSphereArray(i, 0f);
+                }
+                else
+                {
+                    if ((float)i % 2f == 0f)
+                    {
+                        GenerateSphereArray(i, 1f);
+                    }
+                    else
+                    {
+                        GenerateSphereArray(i, -1.5f);
+                    }
+                }
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
         if (objectDatas != null)
         {
-            switch (objectDatas[0][3])
-            {
-                case 0f:
-                case 2f:
-                    GenerateSphere(objectDatas[0][0], objectDatas[0][1], objectDatas[0][2], 0f);
-                    break;
-                case 1f:
-                    GenerateSphere(0f, objectDatas[0][0], objectDatas[0][1], 1f);
-                    break;
-            }
-            objectDatas[0] = null;
+            GenerateSphere(objectDatas[0][0], objectDatas[0][1], objectDatas[0][2], objectDatas[0][3]);
+        }
+        */
+    }
+
+    void GenerateSphereArray(int i, float x)
+    {
+        GenerateSphere(-2f + x, 20f + (i - 1) * 10, 1f, 0f);
+        GenerateSphere(2f + x, 20f + (i - 1) * 10, 1f, 0f);
+        if ((float)i % 3f == 0f)
+        {
+            GenerateSphere(0f + x, 20f + (i - 1) * 10, 1f, 0f);
         }
     }
 
     void GenerateSphere(float x, float z, float number, float type)
     {
-        EachSphere eachSphere = cloneObject.GetComponent<EachSphere>();
         switch (type)
         {
             case 0:
                 cloneObject = Instantiate(spherePrefab, new Vector3(x, 0.5f, z), Quaternion.identity);
-                eachSphere.Create((int)number);
                 break;
             case 1:
                 cloneObject = Instantiate(wallPrefab, new Vector3(0f, 7f, z), Quaternion.identity, wallPack.transform);
-                eachSphere.Create((int)number);
                 break;
             case 2:
                 cloneObject = Instantiate(spherePrefab, new Vector3(x, 0.5f, z), Quaternion.identity);
+                break;
+            default:
+                break;
+        }
+        EachSphere eachSphere = cloneObject.GetComponent<EachSphere>();
+        switch (type)
+        {
+            case 0:
+                eachSphere.Create((int)number);
+                break;
+            case 1:
+                eachSphere.Create((int)number);
+                break;
+            case 2:
                 eachSphere.Create((int)number);
                 cloneObject = Instantiate(thornPrefab, new Vector3(x, -0.4f, z + 2.0f), Quaternion.Euler(35, 0, 45));
                 break;
